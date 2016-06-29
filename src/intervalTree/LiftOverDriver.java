924ada08ae8fc8ab3a094e5fec79c3ad2aa2bf71
manojbhosale <manoj.bhosale31@gmail.com>
2016-06-29 08:02:35 +0530

    chromosomeStrandFix

--- src/intervalTree/LiftOverDriver.java
+++ src/intervalTree/LiftOverDriver.java
@@ -5,8 +5,11 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
@@ -21,20 +24,20 @@ public class LiftOverDriver {
 		Chain chn = new Chain();
 		//LiftChain lc = new LiftChain();
 
-		List<Chain> chains = chn.liftOverChain(new File("C:\\Manoj\\Progamming\\JAVA\\jLiftOver\\hg19ToHg18.over.chain")); 
+		List<Chain> chains = chn.liftOverChain(new File("C:\\Manoj\\Progamming\\JAVA\\jLiftOver\\hg38Test\\hg19ToHg38.over.chain")); 
 
 		HashMap<String,IntervalTree<List<Object>>> chainIndex = chn.indexChains(chains);
 		/*BufferedReader br = new BufferedReader(new FileReader("C:\\Manoj\\Progamming\\JAVA\\jLiftOver\\testOneSeqCovered_Small.bed"));
-		
 
 
 
-*/
+
+		 */
 		SortedSet<BedInterval> res = new TreeSet<>();
-		PrintWriter pw=  new PrintWriter("C:\\Manoj\\Progamming\\JAVA\\jLiftOver\\testResSmall.bed");
-
+		//PrintWriter pw=  new PrintWriter("C:\\Manoj\\Progamming\\JAVA\\jLiftOver\\testResSmall.bed");
+		//chr1	13001387	13001506
 		try{
-			res = chn.convertCoordinate(chainIndex,"chr8", 17440060, 17440260, "+");
+			res = chn.convertCoordinate(chainIndex,"chr1", 144160244, 144160368, "+");
 		}catch(ChainException e){
 			e.printChainException();
 		}catch(Exception e1){
@@ -43,8 +46,8 @@ public class LiftOverDriver {
 
 		for(BedInterval i : res){
 
-			pw.println(i.getChromosome()+"\t"+i.getStart()+"\t"+i.getStop());
-
+			//pw.println(i.getChromosome()+"\t"+i.getStart()+"\t"+i.getStop()+"\t"+i.getStrand());
+			System.out.println(i.getChromosome()+"\t"+i.getStart()+"\t"+i.getStop()+"\t"+i.getStrand());
 		}
 
 
@@ -70,22 +73,22 @@ public class LiftOverDriver {
 			}
 
 		}	
-
-
 		br.close();
 		pw.close();*/
-		//LiftOverDriver lod = new LiftOverDriver();
-		//lod.liftBed(chainIndex, new File("C:\\Manoj\\Progamming\\JAVA\\jLiftOver\\TestData\\hg19.rand.bed"),new File("C:\\Manoj\\Progamming\\JAVA\\jLiftOver\\CrossMaptestDatahg19to18.bed"));
 
+	/*		LiftOverDriver lod = new LiftOverDriver();
+		lod.liftBed(chainIndex, new File("C:\\Manoj\\Progamming\\JAVA\\jLiftOver\\hg38Test\\OneSeq CNV Backbone_analyzableTarget.bed"),new File("C:\\Manoj\\Progamming\\JAVA\\jLiftOver\\hg38Test\\multipleOff95.bed"));
+		*/
 	}
 
 
 	public void liftBed(HashMap<String,IntervalTree<List<Object>>> chainIndex,File inbed, File outBed) throws Exception{
-
+		boolean reportMultiple = false;
+		double inputRemapRatio = 0.95;
 		PrintWriter pw = new PrintWriter(outBed);
 		BufferedReader br = new BufferedReader(new FileReader(inbed));
 		Chain chn = new Chain();
-
+		PrintWriter pwfail = new PrintWriter(outBed.getParent()+"\\"+outBed.getName()+".failed");
 
 		String line = "";
 		while((line = br.readLine())!= null){
@@ -123,13 +126,77 @@ public class LiftOverDriver {
 				SortedSet<BedInterval> res = new TreeSet<>();
 				res = chn.convertCoordinate(chainIndex, chromosome, start, stop, strand);
 
-				pw.println(line);
-				for(BedInterval inter : res){
+				String selectedCombi = selectStrandAndChromosome(res);
 
-					pw.print(inter.getChromosome()+"\t"+inter.getStart()+"\t"+inter.getStop());
+				String[] chrStrand = selectedCombi.split("\t");
+
+
+
+				Integer queryBaseSize = stop - start;
+				Integer remapBaseSize = 0; 
+				for(BedInterval inter : res){
+					if(inter.getChromosome().equals(chrStrand[0]) && inter.getStrand().equals(chrStrand[1])){	
+						remapBaseSize = remapBaseSize + inter.getStop() - inter.getStart();
+					}
+				}
+
+				double remapRatio = (double)remapBaseSize/queryBaseSize;
+
+
+				if(remapRatio < inputRemapRatio){
+
+					pwfail.println(line);
+					System.out.println(line+"\t"+remapRatio+"\t"+remapBaseSize+"\t"+queryBaseSize);
+					continue;
+
+				}
+				//pw.println(line);
+				//System.out.println(line+"\t"+remapRatio+"\t"+remapBaseSize+"\t"+queryBaseSize);
+				if(reportMultiple){
+
+					for(BedInterval inter : res){
+						if(inter.getChromosome().equals(chrStrand[0]) && inter.getStrand().equals(chrStrand[1])){
+							pw.print(inter.getChromosome()+"\t"+inter.getStart()+"\t"+inter.getStop()+"\t"+inter.getStrand());
+							for(int i = 3; i < splited.length ; i++){
+								if(i == strandIndex){
+									pw.print("\t"+inter.getStrand());
+								}else{
+									pw.print("\t"+splited[i]);
+								}
+							}
+						}
+						pw.print("\n");
+
+					}
+				}else{
+
+					/*List<BedInterval> plus = new ArrayList<>();
+					List<BedInterval> minus = new ArrayList<>();
+
+					// segregate strand
+
+					for(BedInterval i : res){
+
+						if(i.getStrand().equals("+")){
+							plus.add(i);
+						}else if(i.getStrand().equals("-")){
+							minus.add(i);
+						}else{
+							throw new ChainException("Invalid strand!!");
+						}
+
+					}
+					 */
+					BedInterval firstInterval = res.first();
+					BedInterval lastInterval = res.last();
+
+					Integer combinedStart = firstInterval.getStart();
+					Integer combinedStop = lastInterval.getStop();
+
+					pw.print(firstInterval.getChromosome()+"\t"+combinedStart+"\t"+combinedStop+"\t"+firstInterval.getStrand());
 					for(int i = 3; i < splited.length ; i++){
 						if(i == strandIndex){
-							pw.print("\t"+inter.getStrand());
+							pw.print("\t"+firstInterval.getStrand());
 						}else{
 							pw.print("\t"+splited[i]);
 						}
@@ -142,8 +209,80 @@ public class LiftOverDriver {
 
 		}
 		pw.close();
+		pwfail.close();
 
 
 	}
 
+
+	public String selectStrandAndChromosome(SortedSet<BedInterval> res){
+
+		Map<String, Integer> chrStrandMap = new HashMap<>();
+		String selectedCombi = "";
+		for(BedInterval i : res){
+
+			String tempStr = i.getChromosome()+"\t"+i.getStrand();
+
+
+			if(chrStrandMap.containsKey(tempStr)){
+
+				Integer size = chrStrandMap.get(tempStr);
+				Integer newIntervalSize = i.getStop() - i.getStart();
+				size = size + newIntervalSize;
+
+				chrStrandMap.put(tempStr, size);
+
+			}else{
+				Integer newIntervalSize = i.getStop() - i.getStart();
+				chrStrandMap.put(tempStr,newIntervalSize);
+			}
+
+
+			Integer oldSize = 0;
+
+			//List<Integer> allSizes = chrStrandMap.values();
+
+
+			for(String combi : chrStrandMap.keySet()){
+				Integer newSize = chrStrandMap.get(combi);
+				if(newSize > oldSize){
+					oldSize = newSize;
+				}
+			}
+
+
+
+			for(String combi : chrStrandMap.keySet()){
+				if(chrStrandMap.get(combi) == oldSize){
+					selectedCombi = combi;
+				}
+			}
+
+			/*	if(chrStrandMap.containsKey(i.getStrand())){
+				Map<String,Integer> chrMap = chrStrandMap.get(i.getStrand());
+				if(chrMap.containsKey(i.getChromosome())){
+					Integer size = chrMap.get(i.getChromosome());
+					Integer newSize = i.getStop() - i.getStart();
+					size = size + newSize;
+					chrMap.put(i.getChromosome(), size);
+				}else{
+					Integer newSize = i.getStop() - i.getStart();
+					chrMap.put(i.getChromosome(), newSize);
+				}
+
+			}else{
+				Map<String,Integer> chrMap = new HashMap<>();
+				Integer newSize = i.getStop() - i.getStart();
+				chrMap.put(i.getChromosome(),newSize);
+				chrStrandMap.put(i.getStrand(),chrMap);
+			}
+			 */	
+
+		}
+		return selectedCombi;
+
+	}
+
 }
+
+
